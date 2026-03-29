b0c980455079887d42f54a6b10d719b772df75fc
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2026-03-29 16:36:50 +0200

    idiomatic class definition

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 const props = defineProps({
-    className: {
+    class: {
         type: String,
         default: ''
     }
 })
-
-const componentClass = `bg-slate-100 p-4 shadow-lg rounded border border-slate-200 ${props.className}`
 </script>
 
 <template>
-    <section :class="componentClass">
+    <section :class="['bg-slate-100 p-4 shadow-lg rounded border border-slate-200', props.class]">
         <slot></slot>
     </section>
 </template>
