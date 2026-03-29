52e44381a7cba74fa0ee9561eccd7abe6201e242
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2026-03-29 16:09:31 +0200

    make it more idiomatic - use vue components with two way binding

---
+++ src/components/app-input/AppInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { InputTypeHTMLAttribute } from 'vue';
+
+defineProps<{
+    id: string,
+    type: InputTypeHTMLAttribute,
+    step?: string
+}>()
+const model = defineModel()
+</script>
+
+<template>
+    <label :for="id">Kwota zaalokowana na lokacie</label>
+    <br />
+    <input :id="id" v-model="model" :type="type" :step="step" />
+    <br />
+</template>
--- src/components/deposit/DepositCalculator.vue
+++ src/components/deposit/DepositCalculator.vue
@@ -3,67 +3,30 @@ import { formatDate } from '@/shared/utils/formatDate';
 import { computed, ref } from 'vue';
 import { getDepositEndDate } from './utils/getDepositEndDate';
 import { getDepositGains } from './utils/getDepositGains';
+import AppInput from '../app-input/AppInput.vue';
 
-
-const amount = ref('1000')
+const amount = ref(1000)
 const startDate = ref("2026-01-19")
-const annualInterest = ref('2')
-const periodMonths = ref('6')
+const annualInterest = ref(2)
+const periodMonths = ref(6)
 
-const form = computed(() => ({
-    amount: Number(amount.value),
-    startDate: new Date(startDate.value),
-    annualInterest: Number(annualInterest.value),
-    periodMonths: Number(periodMonths.value)
+const depositEndDate = computed(() => getDepositEndDate(new Date(startDate.value), periodMonths.value))
 
+const amountAfterEnd = computed(() => getDepositGains({
+    annualGain: annualInterest.value / 100,
+    amount: amount.value,
+    periodMonths: periodMonths.value,
+    startDate: new Date(startDate.value)
 }))
-
-const depositEndDate = computed(() => getDepositEndDate(form.value.startDate, form.value.periodMonths))
-
-
-const amountAfterEnd = computed(() => {
-    const f = form.value
-    return getDepositGains({
-        annualGain: f.annualInterest / 100,
-        amount: f.amount,
-        periodMonths: f.periodMonths,
-        startDate: f.startDate
-    })
-})
-
-
-const ids = {
-    amount: 'amount',
-    startDate: 'start-date',
-    annualInterest: 'annual-interest',
-    periodMonths: 'periodMonths'
-
-}
-
 </script>
+
 <template>
     <h2>Kalkulator lokat</h2>
 
-    <label :for="ids.amount">Kwota zaalokowana na lokacie</label>
-    <br />
-    <input :id="ids.amount" v-model="amount" type="number" />
-    <br />
-
-    <label :for="ids.startDate">Początek lokaty</label>
-    <br />
-    <input :id="ids.startDate" v-model="startDate" type="date" />
-    <br />
-
-    <label :for="ids.annualInterest">Oprocentowanie w skali roku</label>
-    <br />
-    <input :id="ids.annualInterest" v-model="annualInterest" type="number" step="0.5" />
-    <br />
-
-    <label :for="periodMonths">Ile miesięcy</label>
-    <br />
-    <input :id="periodMonths" v-model="periodMonths" type="number" />
-    <br />
-    <hr />
+    <AppInput id="amount" type="number" v-model.number="amount" />
+    <AppInput id="start-date" type="date" v-model="startDate" />
+    <AppInput id="annual-interest" type="number" v-model.number="annualInterest" step="0.5" />
+    <AppInput id="period-months" type="number" v-model.number="periodMonths" />
 
     <div>
         Liczenie zysku z lokaty na kwotę <b>{{ amount }} zł</b> rozpoczętej <b>{{ startDate }}</b> z
@@ -78,7 +41,7 @@ const ids = {
         </div>
         <div>Zysk netto: {{ amountAfterEnd.netGain }} zł</div>
         <div>Podatki {{ amountAfterEnd.taxes }} zł</div>
-        <div>Depozyt {{ form.amount }} zł</div>
+        <div>Depozyt {{ amount }} zł</div>
     </div>
 
 
