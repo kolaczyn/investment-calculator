58473f057888df9b2fd08b57cc5f93d8c65b7002
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2026-03-31 16:10:26 +0200

    fix type error

--- src/shared/api/depositsApi.ts
+++ src/shared/api/depositsApi.ts
@@ -1,3 +1,4 @@
+import type {DepositDto} from "@/shared/types/DepositDto.ts";
 import { makeGenericApi } from "./makeGenericApi";
 
-export const depositsApi = makeGenericApi("deposits");
+export const depositsApi = makeGenericApi<DepositDto>("deposits");
